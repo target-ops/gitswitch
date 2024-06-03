d9f0e9d5f43d87cfdcbceba5aa13f31af400df29
ofirhaim1 <ofir474@gmail.com>
2024-06-04 00:39:42 +0300

    project restructure

---
+++ src/config.py
@@ -0,0 +1,29 @@
+import os
+import configparser
+
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), '..', 'config.ini')
+
+def load_config():
+    config = configparser.ConfigParser()
+    if os.path.exists(CONFIG_FILE):
+        config.read(CONFIG_FILE)
+    return config
+
+def save_config(config):
+    with open(CONFIG_FILE, 'w') as configfile:
+        config.write(configfile)
+
+def set_current_user(config, vendor, username):
+    if 'current' not in config:
+        config['current'] = {}
+    config['current']['vendor'] = vendor
+    config['current']['username'] = username
+    save_config(config)
+
+def get_current_user(config):
+    if 'current' in config:
+        vendor = config['current']['vendor']
+        username = config['current']['username']
+        return vendor, username
+    else:
+        return None, None
---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def run_command(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        raise Exception(f"Command failed: {stderr.decode('utf-8')}")
+    return stdout.decode('utf-8').strip()
