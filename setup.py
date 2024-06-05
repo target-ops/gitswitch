2e9c071a55ce308334800a8cb0ad849407b2f962
vladi rahmanov <54931373+vrahmanov@users.noreply.github.com>
2024-06-05 09:03:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
-    # name='GitSwitch',
-    # version='0.0.1',
-    # packages=find_packages(include=['src', 'src.*'])
-    # entry_points={
-    #     'console_scripts': ['GitSwitch=GitSwitch.src.main.py:main']
-    # }
+    name='GitSwitch',
+    version='0.0.1',
+    packages=find_packages(include=['src', 'src.*'])
+    entry_points={
+        'console_scripts': ['GitSwitch=GitSwitch.src.main.py:main']
+    }
 )
