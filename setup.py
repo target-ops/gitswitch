621e7b81b41b693c0eea6d4e91711eaa8ba65d9b
ofirhaim1 <ofir474@gmail.com>
2024-06-05 10:44:10 +0300

    adding setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
+
 setup(
-    name='GitSwitch',
-    version='0.0.1',
-    packages=find_packages(include=['src', 'src.*'])
+    name="GitSwitch",
+    version="0.0.1",
+    author="OfirHaim",
+    author_email="ofir474@gmail.com",
+    description="Manage multiple Git users for different vendors.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/target-ops/GitSwitch.git",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     entry_points={
         'console_scripts': ['GitSwitch=GitSwitch.src.main.py:main']
     }
